20481acabb94fe4edfa773ad932e1999ee9f516c
limeu <limeu@DESKTOP-KFBSC5A>
2021-11-01 14:47:15 +0900

    임윤정/기반/멤버VO추가

---
+++ CloudProject/src/main/java/com/service/cloud/MemberVO.java
@@ -0,0 +1,13 @@
+package com.service.cloud;
+
+public class MemberVO {
+	private String memberNum;
+	private String pwd;
+	private String name;
+	private String phone;
+	private String position;
+	private String deparment;
+	private String memberAuth;
+
+	
+}
