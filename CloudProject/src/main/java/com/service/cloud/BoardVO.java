fb6a326c09fb00eefff8b5287ac997b986becdaf
limeu <limeu@DESKTOP-KFBSC5A>
2021-11-01 15:03:14 +0900

    임윤정/초기VO작성/setter,getter,toString까지 작성

--- CloudProject/src/main/java/com/service/cloud/BoardVO.java
+++ CloudProject/src/main/java/com/service/cloud/BoardVO.java
@@ -1,5 +1,31 @@
 package com.service.cloud;
 
+import java.sql.Date;
+
 public class BoardVO {
 
+	private String contentNum;
+	private String boardTitle;
+	private String boardContent;
+	private Date boardDate;
+	public void setContentNum(String contentNum) {
+		this.contentNum = contentNum;
+	}
+	public void setBoardTitle(String boardTitle) {
+		this.boardTitle = boardTitle;
+	}
+	public void setBoardContent(String boardContent) {
+		this.boardContent = boardContent;
+	}
+	public void setBoardDate(Date boardDate) {
+		this.boardDate = boardDate;
+	}
+	@Override
+	public String toString() {
+		return "BoardVO [contentNum=" + contentNum + ", boardTitle=" + boardTitle + ", boardContent=" + boardContent
+				+ ", boardDate=" + boardDate + "]";
+	}
+	
+
+	
 }
--- CloudProject/src/main/java/com/service/cloud/MemberVO.java
+++ CloudProject/src/main/java/com/service/cloud/MemberVO.java
@@ -8,6 +8,32 @@ public class MemberVO {
 	private String position;
 	private String deparment;
 	private String memberAuth;
+	public void setMemberNum(String memberNum) {
+		this.memberNum = memberNum;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	public void setDeparment(String deparment) {
+		this.deparment = deparment;
+	}
+	public void setMemberAuth(String memberAuth) {
+		this.memberAuth = memberAuth;
+	}
+	@Override
+	public String toString() {
+		return "MemberVO [memberNum=" + memberNum + ", pwd=" + pwd + ", name=" + name + ", phone=" + phone
+				+ ", position=" + position + ", deparment=" + deparment + ", memberAuth=" + memberAuth + "]";
+	}
 
 	
 }
